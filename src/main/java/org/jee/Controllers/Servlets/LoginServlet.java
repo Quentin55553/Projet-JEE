f082566d4c093a65c7c1de4ed9441243a3700bda
Victand <163859192+Victand@users.noreply.github.com>
2024-11-22 17:19:48 +0100

    Update LoginServlet.java

--- src/main/java/org/jee/Controllers/Servlets/LoginServlet.java
+++ src/main/java/org/jee/Controllers/Servlets/LoginServlet.java
@@ -1,15 +1,17 @@
-package org.jee.Servlets;
+package org.jee.Controllers.Servlets;
 
-import org.jee.Util.HibernateUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+
 import org.hibernate.Session;
 import org.hibernate.query.Query;
+
 import org.jee.entity.Personne;
+import org.jee.Util.HibernateUtil;
 
 import java.io.IOException;
 
@@ -21,13 +23,12 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         // Vérifier les informations d'identification via Hibernate
-        Session session = org.jee.Util.HibernateUtil.getSessionFactory().openSession();
+        Session session = HibernateUtil.getSessionFactory().openSession();
         Query query = session.createQuery("FROM Personne WHERE idPersonne = :idPersonne AND password = :password");
         query.setParameter("idPersonne", idPersonne);
         query.setParameter("password", password);
 
         Personne personne = (Personne) query.uniqueResult();
-        System.out.println(personne);
         session.close();
 
         if (personne != null) {
@@ -35,12 +36,25 @@ public class LoginServlet extends HttpServlet {
             HttpSession httpSession = request.getSession();
             httpSession.setAttribute("user", personne);
 
-            // Redirige vers la page d'accueil
-            response.sendRedirect("home.jsp");
+            // Redirige en fonction du rôle
+            switch (personne.getRole()) {
+                case 1: // Rôle étudiant
+                    response.sendRedirect("admin/home.jsp");
+                    break;
+                case 2: // Rôle enseignant
+                    response.sendRedirect("teacher/home.jsp");
+                    break;
+                case 3: // Rôle administrateur
+                    response.sendRedirect("student/home.jsp");
+                    break;
+                default:
+                    // Rôle inconnu
+                    response.sendRedirect("Vue/login.jsp?error=true");
+                    break;
+            }
         } else {
             // Authentification échouée
-            response.sendRedirect("login.jsp?error=true");
+            response.sendRedirect("Vue/login.jsp?error=true");
         }
     }
-
 }
