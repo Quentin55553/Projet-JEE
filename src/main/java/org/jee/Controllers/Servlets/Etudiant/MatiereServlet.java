0cd68e32def88ed6a81d998d4b301f0596fb91c4
YouennBogaer <163859975+YouennBogaer@users.noreply.github.com>
2024-11-28 17:36:44 +0100

    javadoc

--- src/main/java/org/jee/Controllers/Servlets/Etudiant/MatiereServlet.java
+++ src/main/java/org/jee/Controllers/Servlets/Etudiant/MatiereServlet.java
@@ -17,45 +17,43 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Serlvet utilisé pour afficher les détails des notes d'une matière par l'étudiant depuis la page menu_Etudiant.jsp
+ */
 @WebServlet("/MatiereServlet")
 public class MatiereServlet extends HttpServlet {
 
+    /**
+     * Etablie la liste des resultats d'un étudiant sur le Cours spécifié et redirige l'étudiant vers les détails des
+     * notes du cours.
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Get the course ID from the request parameter
         int courseId = Integer.parseInt(request.getParameter("idCours"));
 
-        // Retrieve the user (student) from the session
         Personne user = (Personne) request.getSession().getAttribute("user");
 
-        // Open Hibernate session
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Transaction tx = session.beginTransaction();
 
-            // Retrieve the course with the given ID
             Cours cours = session.get(Cours.class, courseId);
 
-            // If the course is not found, redirect to the student menu
             if (cours == null) {
                 response.sendRedirect(request.getContextPath() + "/MenuEtudiantServlet");
                 return;
             }
 
-            // Query for the results (notes) of the current student for this course
             String hql = "FROM Resultat r WHERE r.coursByIdCours.idCours = :courseId AND r.personneByIdEtudiant.idPersonne = :studentId";
             Query<Resultat> query = session.createQuery(hql, Resultat.class);
             query.setParameter("courseId", courseId);
             query.setParameter("studentId", user.getIdPersonne());
             List<Resultat> resultats = query.list();
 
-            // Set the course and results as request attributes
             request.setAttribute("cours", cours);
             request.setAttribute("resultats", resultats);
 
-            // Commit transaction
             tx.commit();
 
-            // Forward the request to the JSP page
             request.getRequestDispatcher("/Vue/Etudiant/note_Matiere.jsp").forward(request, response);
 
         } catch (Exception e) {
